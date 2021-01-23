8798da249c510d664620cf698a95e874c38a02af
Erik Fahlén <erfa90@gmail.com>
2021-01-23 01:08:41 +0100

    Shrink the MIDI controller program drastically to fit the 128 byte dynamic memory of the ATTiny2313.

--- midi-controller/midi_controller/midi_controller.ino
+++ midi-controller/midi_controller/midi_controller.ino
@@ -1,33 +1,75 @@
-#include <Button.h>
+const byte MIDI_CC_COMMAND = 0xB0;
 
-const int NOTE_OFF = 0x80;
-const int NOTE_ON = 0x90;
-const int CC = 0xB0;
+// CC 16-19 Are "General Purpose"
+const byte CC1 = 16;
+const byte CC2 = 17;
+const byte CC3 = 18;
 
-Button button1(2);
-Button button2(4);
-Button button3(7);
+const byte VELOCITY = 127;
+
+const int BAUD_RATE = 31250;
+
+const byte BUTTON_PIN1 = 2;
+const byte BUTTON_PIN2 = 4;
+const byte BUTTON_PIN3 = 6;
+
+const byte DEBOUNCE = 50;
+
+class Button {
+public:
+  Button()
+    : m_previousState(HIGH)
+    , m_currentState(HIGH)
+  {
+  }
+
+  void begin(byte pin) {
+    pinMode(pin, INPUT_PULLUP);
+  }
+
+  void update(byte pin) {
+    m_previousState = m_currentState;
+    m_currentState = digitalRead(pin);
+
+    if (m_previousState != m_currentState) {
+      delay(DEBOUNCE);
+    }
+  }
+
+  bool isPressed() {
+    return m_currentState == LOW && m_previousState == HIGH;
+  }
+
+private:
+  int m_previousState;
+  int m_currentState;
+};
+
+Button button1;
+Button button2;
+Button button3;
 
 void setup() {
-  button1.begin();
-  button2.begin();
-  button3.begin();
+  Serial.begin(BAUD_RATE);
 
-  Serial.begin(31250);
-  pinMode(13, OUTPUT);
+  button1.begin(BUTTON_PIN1);
+  button2.begin(BUTTON_PIN2);
+  button3.begin(BUTTON_PIN3);
 }
 
 void loop() {
-  // CC 16-19 Are "General Purpose"
+  button1.update(BUTTON_PIN1);
+  button2.update(BUTTON_PIN2);
+  button3.update(BUTTON_PIN3);
 
-  if (button1.pressed()) {
-    midiCommand(CC, 16, 127);
+  if (button1.isPressed()) {
+    midiCommand(MIDI_CC_COMMAND, CC1, VELOCITY);
   }
-  if (button2.pressed()) {
-    midiCommand(CC, 17, 127);
+  if (button2.isPressed()) {
+    midiCommand(MIDI_CC_COMMAND, CC2, VELOCITY);
   }
-  if (button3.pressed()) {
-    midiCommand(CC, 18, 127);
+  if (button3.isPressed()) {
+    midiCommand(MIDI_CC_COMMAND, CC3, VELOCITY);
   }
 }
 
